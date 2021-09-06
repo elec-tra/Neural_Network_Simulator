8659d4b8f703c707deedc89d147cd46828b1a45a
Saranyan <86022504+elec-tra@users.noreply.github.com>
2021-09-06 17:49:42 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,3 @@
-/*
- ============================================================================
- Name        : Neural Netwok Simulator
- Author      : Saranyan
- Version     : 1.0
- Copyright   : Copyright 2020, Saranyan, All rights reserved.
- Description : This is a simple neural network developed in windows forms.
- ============================================================================
- */
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
